46f624bc60a52fcb4a59f1c8a77035be6b6f741f
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-03-29 18:19:53 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -5,35 +5,117 @@
 ////Palidroma
 ////Chiedere all’utente di inserire una parola
 
-var userWord = prompt("Please, insert a word").trim().toLowerCase();
-console.log(userWord);
+// var userWord = prompt("Insert word to check if it's palindrome")
+// 	.trim()
+// 	.toLowerCase();
+// console.log(userWord);
 
-//Creare una funzione per capire se la parola inserita è palindroma
+// //Creare una funzione per capire se la parola inserita è palindroma
 
-var wordReverted = checkPalindrome(userWord);
-console.log(wordReverted);
+// var wordPalindrome = checkPalindrome(userWord);
 
-// if (wordReverted == userWord) {
-// 	console.log("its palindrome");
+// if (userWord === wordPalindrome) {
+// 	console.log("Palindrome");
 // } else {
 // 	console.log("not palindrome");
 // }
 
+// /**
+//  *
+//  * @param {word} word
+//  */
+
+// function checkPalindrome(word) {
+// 	var palindrome = "";
+
+// 	for (var i = word.length - 1; i >= 0; i--) {
+// 		console.log(word[i]);
+
+// 		palindrome += word[i];
+
+// 		console.log(palindrome);
+// 	}
+
+// 	return palindrome;
+// }
+
+//
+//
+//
+// Pari e Dispari
+// L’utente sceglie pari o dispari
+// e inserisce un numero da 1 a 5.
+
+var pariDispariUtente = prompt("Pari o dispari?").trim().toLowerCase();
+
+while (pariDispariUtente != "pari" && pariDispariUtente != "dispari") {
+	pariDispariUtente = prompt("Selezione non valida, scegli pari o dispari")
+		.trim()
+		.toLowerCase();
+}
+console.log(pariDispariUtente);
+
+var numUtente = parseInt(
+	prompt("Scegli un numero da uno a sei").trim().toLowerCase()
+);
+
+while (isNaN(numUtente) || numUtente > 5 || numUtente < 1) {
+	numUtente = parseInt(
+		prompt("Scegli un numero da uno a sei").trim().toLowerCase()
+	);
+}
+console.log(numUtente);
+
+//
+//
+//
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+var numComputer = genNumber(1, 5);
+console.log(numComputer);
+
 /**
  *
- * @param {word} word
+ * @param {num} num numero del computer
  */
+function genNumber(min, max) {
+	var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+	return randomNumber;
+}
 
-function checkPalindrome(word) {
-	var reverse = " ";
+//
+//
+//
+// Sommiamo i due numeri
 
-	for (var i = word.length - 1; i >= 0; i--) {
-		console.log(word[i]);
+var res = numUtente + numComputer;
+console.log(res);
 
-		reverse += word[i];
+//
+//
+//
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
-		console.log(reverse);
+function pariDispari(num) {
+	if (num % 2 == 0) {
+		return "pari";
+	} else {
+		return "dispari";
 	}
+}
 
-	return reverse;
+var res = pariDispari(res);
+
+console.log(res);
+
+//
+//
+//
+// Dichiariamo chi ha vinto.
+//
+
+if (res == pariDispariUtente) {
+	console.log("Hai vinto!");
+} else {
+	console.log("Hai perso!");
 }
